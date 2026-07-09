ff3e98a2a921462448d981deb3128d2f0cbe5fde
abdullah-621 <abdullah.masum.cse@gmail.com>
2026-07-09 07:26:02 +0600

    Basics structure

---
+++ Basics/basics.sql
@@ -0,0 +1,17 @@
+create database if not exists collage;
+
+USE collage; 
+
+create table if not exists students(
+	id int primary key,
+    name varchar(50),
+    age int not null
+    );
+
+INSERT into students values(1, "Masum", 22);
+INSERT into students values(2, "Noman", 17);
+
+select * FROM students;
+
+SHOW databases;
+SHOW tables;
